f872dee7e042d84556c34436394a24008b48887f
Nourhan Adel <91224846+NorhanAdel@users.noreply.github.com>
2023-07-08 02:10:12 +0300

    Add files via upload

---
+++ src/pages/UplaodCertifiacte/UploadCertificate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ButtonTop from '../../components/ButtonTop/ButtonTop';
+import Background from '../../components/Background/Background';
+import Form2 from '../../components/Form2';
+
+const UplaodCertifiacte=()=>{
+return(
+<div>
+<Background/>
+  <Form2/>
+      <ButtonTop/>
+</div>
+)
+}
+export default UplaodCertifiacte;
