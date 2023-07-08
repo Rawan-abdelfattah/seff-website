a194eb70153634ddfe46afae7d424d8ab78919fd
Rana <52126542+ranamagdi@users.noreply.github.com>
2023-07-08 04:16:07 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import Header from './components/Header/Header';
-import UploadCertificate from './pages/UplaodCertifiacte/UploadCertificate';
-import Footer from './components/Footer/Footer';
-
-
+import { Routes,Route,HashRouter} from 'react-router-dom';
+import Student_Portal from './Pages/Student_Portal/Student_Portal';
+import Instructor_Portal from './Pages/Instructor_Portal/Instructor_Portal'
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <UploadCertificate />
-      <Footer />
+    <div >
+     <HashRouter>
+      <Routes>
+      <Route path="/" element={<Student_Portal/>}/>
+      {/* <Route path="/" element={<Instructor_Portal/>}/> */}
+      </Routes>
+    </HashRouter>
+
+
     </div>
   );
 }
